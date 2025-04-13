dff6b146609256a6bd7375a30d160f8d174904c1
Jack Carmichael <carmichaeljr@outlook.com>
2025-04-12 19:37:53 -0600

    added comments

--- bs/bs.go
+++ bs/bs.go
@@ -8,6 +8,8 @@ import (
 	sbbs "github.com/barbell-math/smoothbrain-bs"
 )
 
+// Generate a stage that runs `git diff` and returns an error if there are any
+// differences. This is mainly used by ci targets.
 func gitDiffStage(errMessage string, targetToRun string) sbbs.StageFunc {
 	return sbbs.Stage(
 		"Run Diff",
@@ -31,6 +33,9 @@ func gitDiffStage(errMessage string, targetToRun string) sbbs.StageFunc {
 }
 
 func main() {
+	// Register a target that updates all dependences. Dependencies that are in
+	// the `barbell-math` repo will always be pinned at latest and all other
+	// dependencies will be updated to the latest version.
 	sbbs.RegisterTarget(
 		context.Background(),
 		"updateDeps",
@@ -125,12 +130,16 @@ func main() {
 		),
 	)
 
+	// Registers a target that will update all deps and run a diff to make sure
+	// that the commited code is using all of the correct dependencies.
 	sbbs.RegisterTarget(
 		context.Background(),
 		"ciCheckDeps",
 		sbbs.TargetAsStage("updateDeps"),
 		gitDiffStage("Out of date packages were detected", "updateDeps"),
 	)
+	// Registers a target that will install gomarkdoc, update the readme, and
+	// run a diff to make sure that the commited readme is up to date.
 	sbbs.RegisterTarget(
 		context.Background(),
 		"ciCheckReadme",
@@ -138,12 +147,19 @@ func main() {
 		sbbs.TargetAsStage("updateReadme"),
 		gitDiffStage("Readme is out of date", "updateReadme"),
 	)
+	// Registers a target that will run go fmt and then run a diff to make sure
+	// that the commited code is properly formated.
 	sbbs.RegisterTarget(
 		context.Background(),
 		"ciCheckFmt",
 		sbbs.TargetAsStage("fmt"),
 		gitDiffStage("Fix formatting to get a passing run!", "fmt"),
 	)
+	// Registers a target that will run all mergegate checks. This includes:
+	//	- checking that the code is formatted
+	//	- checking that the readme is up to date
+	//	- checking that the dependencies are up to date
+	//	- checking that all unit tests pass
 	sbbs.RegisterTarget(
 		context.Background(),
 		"mergegate",
