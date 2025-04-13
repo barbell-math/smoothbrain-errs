ee78a1529cb34b52b252307f1a2320f75f2b5e0c
Jack Carmichael <carmichaeljr@outlook.com>
2025-04-12 19:06:38 -0600

    fixed old unit test references

--- Errs_test.go
+++ Errs_test.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/barbell-math/smoothbrain-test"
+	sbtest "github.com/barbell-math/smoothbrain-test"
 )
 
 var testErrOne = errors.New("err1")
@@ -13,14 +13,14 @@ var testErrTwo = errors.New("err2")
 
 func TestInverseWrap(t *testing.T) {
 	e := InverseWrap(testErrOne, "%d", 100)
-	smoothbraintest.Eq(t,
+	sbtest.Eq(t,
 		fmt.Sprintf("100\n  |- %s", testErrOne.Error()), e.Error(),
 	)
 }
 
 func TestWrap(t *testing.T) {
 	e := Wrap(testErrOne, "%d", 100)
-	smoothbraintest.Eq(t,
+	sbtest.Eq(t,
 		fmt.Sprintf("%s\n  |- 100", testErrOne.Error()), e.Error(),
 	)
 }
@@ -31,7 +31,7 @@ func TestWrapValueList(t *testing.T) {
 		"Val outside range",
 		WrapListVal{ItemName: "value", Item: 10},
 	)
-	smoothbraintest.Eq(
+	sbtest.Eq(
 		t,
 		fmt.Sprintf(
 			"%s\n  |- Description: Val outside range\n  |- value (int): 10",
@@ -43,13 +43,13 @@ func TestWrapValueList(t *testing.T) {
 
 func TestUnwrap(t *testing.T) {
 	e := Wrap(testErrOne, "%d", 100)
-	smoothbraintest.Eq(t, testErrOne, Unwrap(e))
-	smoothbraintest.ContainsError(t, testErrOne, Unwrap(e))
+	sbtest.Eq(t, testErrOne, Unwrap(e))
+	sbtest.ContainsError(t, testErrOne, Unwrap(e))
 }
 
 func TestAppendErrorTwoErrors(t *testing.T) {
 	e := AppendError(testErrOne, testErrTwo)
-	smoothbraintest.Eq(
+	sbtest.Eq(
 		t,
 		fmt.Sprintf(
 			"%s\n%s",
@@ -62,17 +62,17 @@ func TestAppendErrorTwoErrors(t *testing.T) {
 
 func TestAppendErrorOnlyFirst(t *testing.T) {
 	e := AppendError(testErrOne, nil)
-	smoothbraintest.Eq(t, testErrOne.Error(), e.Error())
-	smoothbraintest.Eq(t, testErrOne, e)
+	sbtest.Eq(t, testErrOne.Error(), e.Error())
+	sbtest.Eq(t, testErrOne, e)
 }
 
 func TestAppendErrorOnlySecond(t *testing.T) {
 	e := AppendError(nil, testErrOne)
-	smoothbraintest.Eq(t, testErrOne.Error(), e.Error())
-	smoothbraintest.Eq(t, testErrOne, e)
+	sbtest.Eq(t, testErrOne.Error(), e.Error())
+	sbtest.Eq(t, testErrOne, e)
 }
 
 func TestAppendErrorBothNil(t *testing.T) {
 	e := AppendError(nil, nil)
-	smoothbraintest.Nil(t, e)
+	sbtest.Nil(t, e)
 }
